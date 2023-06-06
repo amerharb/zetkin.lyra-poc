d3d2f1a74fcad1b099334e3dad826ab9a5ee99ee
Amer Harb <amer.harb@gmail.com>
2023-06-06 16:06:16 +0200

    example of simple git use

--- extractor/index.ts
+++ extractor/index.ts
@@ -1,1 +1,15 @@
-console.log('foo');
+import { simpleGit } from 'simple-git'
+
+// main function
+async function main() {
+  const git = simpleGit('../mock-repo/')
+  const log = await git.log()
+  console.log(log)
+  for (const l of log.all) {
+    console.log(l)
+    await new Promise((resolve) => setTimeout(resolve, 500))
+  }
+}
+
+// run main function
+main()
