87e68b33b303a9ad2d117ef7fde0326433d13442
Richard Olsson <r@richardolsson.se>
2023-06-06 16:11:43 +0200

    Crudely read messageIds at all states in history

--- extractor/index.ts
+++ extractor/index.ts
@@ -1,1 +1,32 @@
-console.log('foo');
+import simpleGit from 'simple-git';
+
+const MSG_FILE = 'src/features/smartSearch/l10n/messageIds.ts';
+const PATH = process.argv[2];
+
+import moduleAlias from 'module-alias';
+moduleAlias.addPath('../mock-repo/src');
+
+(async () => {
+  const git = simpleGit({
+    baseDir: PATH,
+  });
+  const commits = await git.log([MSG_FILE]);
+  const history = commits.all
+    .concat()
+    .sort(
+      (left, right) =>
+        new Date(left.date).getTime() - new Date(right.date).getTime()
+    );
+
+  for (const commit of history) {
+    console.log('----------------------------------------');
+    console.log('messages at', commit.date);
+    await git.checkout(commit.hash);
+
+    // Dynamically import message map
+    const modulePath = PATH + MSG_FILE;
+    const messageIdsImport = await import(modulePath);
+    const messageIds = messageIdsImport.default;
+    console.log(messageIds);
+  }
+})();
